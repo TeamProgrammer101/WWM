0ded5de343990e9c4f753e460c7328f761777ede
LeahKwon <dosing87@naver.com>
2016-07-05 16:56:00 +0900

    [Heekyum]ios tabbar

---
+++ IOS/WWM/WWM/MeViewController.m
@@ -0,0 +1,27 @@
+//
+//  MeViewController.m
+//  WWM
+//
+//  Created by LeahKwon on 2016. 7. 5..
+//  Copyright © 2016년 LeahKwon. All rights reserved.
+//
+
+#import "MeViewController.h"
+
+@interface MeViewController ()
+
+@end
+
+@implementation MeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ IOS/WWM/WWM/WorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  WorldViewController.h
+//  WWM
+//
+//  Created by LeahKwon on 2016. 7. 5..
+//  Copyright © 2016년 LeahKwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorldViewController : UIViewController
+
+
+@end
+
